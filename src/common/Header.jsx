e5d63815264b1bcae9cbea243bd144029b4cb68e
RakshitGupta19 <krish.aggarwal212@gmail.com>
2025-02-21 12:29:24 +0530

    changes done
    made header sticky and adjusted the size

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -3,22 +3,24 @@ import './Header.css';
 
 function Header(){
     return(
-        <div className='navbar'>
-            <div className="navbarImg">
-                <img src="https://res.cloudinary.com/dznnm9b1j/image/upload/v1740030603/Kurit_dxaoxt.png" alt="" />
-            </div>
-            <div className="navbarItems">
-                <a href="#"><span>Home</span></a>
-                <a href="#"><span>About</span></a>
-                <a href="#"><span>Contact</span></a>
-            </div>
-            <div className="navbarContact">
-                <div className="contactUsHeading">
-                    <p>Contact Us</p>
+        <div className="mainNavbar">
+            <div className='navbar'>
+                <div className="navbarImg">
+                    <img src="https://res.cloudinary.com/dznnm9b1j/image/upload/v1740030603/Kurit_dxaoxt.png" alt="" />
                 </div>
-                <div className="navbarContactNumber">
-                    <p>+91 95177 77992</p>
-                    <img src="https://res.cloudinary.com/dznnm9b1j/image/upload/v1740036744/up-right-arrow_ovtgwy.png" alt="" />
+                <div className="navbarItems">
+                    <a href="#"><span>Home</span></a>
+                    <a href="#"><span>About</span></a>
+                    <a href="#"><span>Contact</span></a>
+                </div>
+                <div className="navbarContact">
+                    <div className="contactUsHeading">
+                        <p>Contact Us</p>
+                    </div>
+                    <div className="navbarContactNumber">
+                        <p>+91 95177 77992</p>
+                        <img src="https://res.cloudinary.com/dznnm9b1j/image/upload/v1740036744/up-right-arrow_ovtgwy.png" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
